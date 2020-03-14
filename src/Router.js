b7ae4c2d236b8a3b3f4b270946c53c1b1649b5a6
Carlos Fabián <carl.fab.g@gmail.com>
2020-03-14 15:25:19 -0600

    se creo el Router.js con las rutas de las paginas de la app pero aun no es llamado en index.js

---
+++ src/Router.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+
+
+function Router() {
+    return(
+        <BrowserRouter>
+        <NavBar />  
+         <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/genre/movies' component={Genre}/>
+            <Route exact path='/genre/movies/:id' component={Movie}/>
+        </Switch>
+        < Footer /> 
+        </BrowserRouter>
+    )
+}
+
+export default Router;
---
+++ src/components/GenreCard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Box } from "@chakra-ui/core";
+
+
+class GenreCard extends Component{
+    render(){
+        return(
+<Box  w="20%" p={4} color="white">
+  This is the GenreCard
+</Box>
+        )
+    }
+}
+
+export default GenreCard;
