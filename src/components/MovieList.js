bf8a02c69d030b0920d230037aabd169c7c574d9
Carlos Fabián <carl.fab.g@gmail.com>
2020-03-15 07:50:01 -0600

    se corrigieron estilos del home

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -8,7 +8,7 @@ class MovieList extends Component {
     
     render(){
         return(
-         <SimpleGrid m="10px" mt='10vh' minChildWidth="200px" justify='center' columns={[1, 2, 3]} spacing={10}>
+         <SimpleGrid m="10px" mt='10vh' mb="10vh" minChildWidth="200px" justify='center' columns={[1, 2, 3]} spacing={10}>
               
             {this.props.moviesByGenre.map( (movie, i) => <Link  to={`/genre/movies/detail/${movie.id}`}><MovieCard key={i} movies={movie}/> </Link>)}     
               
