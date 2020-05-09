c2fbba54c1a53e0f703671d71cb1085d2bded3fb
Carlos Fabián <carl.fab.g@gmail.com>
2020-05-08 22:39:25 -0500

    se refactorizo componente SimilarMovies

--- src/components/SimilarMovies.js
+++ src/components/SimilarMovies.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import MovieCard from './MovieCard';
 import { SimpleGrid } from "@chakra-ui/core";
 
 
-class SimilarMovies extends Component{
-    render(){
+function SimilarMovies(props){
+    
         return(
             <SimpleGrid m="10px"mb="13vh" mt='10vh' minChildWidth="200px" justify='center' columns={[1, 2, 3]} spacing={10}>
                  
-               {this.props.similarMovies.map( movie => <Link to={`/genre/movies/detail/${movie.id}`}><MovieCard key={movie.id} movies={movie}/> </Link>)}     
+               {props.similarMovies.map( movie => <Link key={movie.id} to={`/genre/movies/detail/${movie.id}`}><MovieCard  movies={movie}/> </Link>)}     
                  
            </SimpleGrid>
            )
-    }
 };
 
 export default SimilarMovies;
