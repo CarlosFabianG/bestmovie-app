5402a05d29762693360e25a56f4d4000aedc09f0
Carlos Fabián <carl.fab.g@gmail.com>
2020-05-08 22:01:17 -0500

    se refactorizo GenreList de componente de classe a funcion

--- src/components/GenreList.js
+++ src/components/GenreList.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
-import GenreCard from './GenreCard';
-import {Link} from 'react-router-dom';
+import React from 'react'
+import GenreCard from './GenreCard'
+import {Link} from 'react-router-dom'
 import {
     SimpleGrid
   } from '@chakra-ui/core'
 
 
-class GenreList extends Component{
+function GenreList(props){
 
-render(){
     return(
         <SimpleGrid m="10px" 
                     mt='10vh' 
@@ -17,11 +16,11 @@ render(){
                     columns={[1, 2, 3]} 
                     spacing={10}>
           
-         {this.props.genres.map(genre => <Link to={`/genre/movies/${genre.id}`}><GenreCard key={genre.id} genres={genre}/> </Link>)}
-          
-    </SimpleGrid>
+         {props.genres.map(genre => <Link to={`/genre/movies/${genre.id}`} key={genre.id}>
+                                    <GenreCard  genres={genre}/> 
+                                    </Link>)}
+        </SimpleGrid>
     )
 }
-}
 
-export default GenreList;
+export default GenreList
